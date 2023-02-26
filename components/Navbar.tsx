fcdb960fe195f8ec3d47f2c7e08a0036b187d263
Marius Genton <mariusgenton@gmail.com>
2023-02-25 17:38:02 -0800

    Updated navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,23 +1,31 @@
 import styles from '@/styles/Navbar.module.scss'
 import Link from 'next/link'
+import Image from 'next/image';
+
+import CyberLogoLight from '@/public/Images/cyber-logo-light.png';
+import CyberWordmarkLight from '@/public/Images/cyber-wordmark-light.png';
 
 export default function Navbar() {
     return (
         <div className={styles.navbar}>
+        
+        {/* LEFT PART OF THE NAVBAR */}
          <div className="Left">
-             <Link href="/" className="navBarTitle">ACM CYBER</Link>
+            <button style={{all: "unset"}}> {/* TODO: Add onClick() action to go to home page */}
+                <Image src={CyberLogoLight} alt="ACM ACYBER"/>
+                <Image src={CyberWordmarkLight} alt=""/>
+            </button>
          </div>
-
+        
+        {/* SPACER */}
          <div className={styles.spacer}/> 
 
+        {/* RIGHT PART OF THE NAVBAR */}
          <div className="Right">
-             <Link href="/events" className={styles.navbarChildren}>Events</Link>
-             <Link href="/challenges" className={styles.navbarChildren}>Challenges</Link>
-             <Link href="/scoreboard" className={styles.navbarChildren}>Scoreboard</Link>
-             <Link href="/login" className={styles.navbarChildren}>Login</Link>
-             <button className={styles.navbarChildren + " " + styles.registerButton}>
-                <p>Register</p>
-             </button>
+             <Link href="/events">EVENTS</Link>
+             <Link href="/challenges">ABOUT</Link>
+             <Link href="/scoreboard">PBR</Link>
+             <Link href="/login">ARCHIVE</Link>
          </div>
    </div>
     )
