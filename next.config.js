433dd8d500323eb62b8c33384cc520bb59f4b6c0
Benson Liu <bensonhliu@gmail.com>
2023-03-01 12:50:48 -0800

    added image loader

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== "production";
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  assetPrefix: !debug ? "https://cyber.uclaacm.com" : "",
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
