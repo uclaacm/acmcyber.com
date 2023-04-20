f84d2df79eedd7fae48c1ff5ba232d5ed82db804
Benson Liu <bensonhliu@gmail.com>
2023-04-19 22:40:47 -0700

    fixed CI/CD errors

--- next.config.js
+++ next.config.js
@@ -3,12 +3,11 @@ const debug = process.env.NODE_ENV !== "production";
 
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    // loader: "akamai",
-    // path: "/",
-    unoptimized: true
-  },
-  assetPrefix: !debug ? "/" : "",
+  // images: {
+  //   loader: "akamai",
+  //   path: "/",
+  // },
+  assetPrefix: !debug ? "https://cyber.uclaacm.com" : "",
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
