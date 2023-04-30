c3e2952853b28a0b410238fb3638e19506d38443
Benson Liu <bensonhliu@gmail.com>
2023-04-30 10:41:54 -0700

    slimmed styling

--- pages/events.tsx
+++ pages/events.tsx
@@ -1,8 +1,9 @@
+import Head from "next/head";
+import { useState } from "react";
+
 import styles from "@/styles/Events.module.scss";
 
 import AllEvents from "@/data/events";
-import Head from "next/head";
-import React, { useState } from "react";
 
 function eventSquare(id: number) {
   return (
@@ -20,44 +21,38 @@ export default function Events() {
   // display all of the events in the list (hint: use the map() function)
   const [display, setDisplay] = useState(false);
   return (
-    <>
+    <div className="page">
       <Head>
         <title>Events | ACM Cyber at UCLA</title>
       </Head>
-      <div className={styles["home"]}>
-        <div className={styles["page"]}>
-          <h1>EVENTS</h1>
-          <div className={styles["flex"]}>
-            <div className={styles["buttonGrid"]}>
-              <button onClick={() => setDisplay(!display)}>
-                <span>jan 01</span>
-              </button>
-              <button onClick={() => setDisplay(!display)}>
-                <span>jan 02</span>
-              </button>
-              <button onClick={() => setDisplay(!display)}>
-                <span>jan 03</span>
-              </button>
-              <button onClick={() => setDisplay(!display)}>
-                <span>jan 04</span>
-              </button>
-              <button onClick={() => setDisplay(!display)}>
-                <span>jan 05</span>
-              </button>
-              <button onClick={() => setDisplay(!display)}>
-                <span>jan 06</span>
-              </button>
+      <h1>EVENTS</h1>
+      <div className={styles["buttonGrid"]}>
+        <button onClick={() => setDisplay(!display)}>
+          <span>jan 01</span>
+        </button>
+        <button onClick={() => setDisplay(!display)}>
+          <span>jan 02</span>
+        </button>
+        <button onClick={() => setDisplay(!display)}>
+          <span>jan 03</span>
+        </button>
+        <button onClick={() => setDisplay(!display)}>
+          <span>jan 04</span>
+        </button>
+        <button onClick={() => setDisplay(!display)}>
+          <span>jan 05</span>
+        </button>
+        <button onClick={() => setDisplay(!display)}>
+          <span>jan 06</span>
+        </button>
 
-              <div className={display ? styles.blurbShow : styles.blurbHide}>
-                {eventSquare(0)}
-              </div>
-              <div className={display ? styles.blurbShow : styles.blurbHide}>
-                {eventSquare(1)}
-              </div>
-            </div>
-          </div>
+        <div className={display ? styles.blurbShow : styles.blurbHide}>
+          {eventSquare(0)}
+        </div>
+        <div className={display ? styles.blurbShow : styles.blurbHide}>
+          {eventSquare(1)}
         </div>
       </div>
-    </>
+    </div>
   );
 }
