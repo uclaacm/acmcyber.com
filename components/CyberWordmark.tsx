6bcfb18e9e93afa227a28bd3e1fb90c0e96e8ebe
Arc-blroth <45273859+Arc-blroth@users.noreply.github.com>
2023-08-24 22:46:02 -0700

    initial diamond designing
    just don't zoom out and it works (tm)

--- components/CyberWordmark.tsx
+++ components/CyberWordmark.tsx
@@ -4,15 +4,17 @@ import styles from "@/styles/CyberWordmark.module.scss";
 export default function CyberWordmark({
   fontSize,
   lineHeight,
+  className,
   style,
 }: {
-  fontSize: string;
-  lineHeight: string;
+  fontSize?: string;
+  lineHeight?: string;
+  className?: string;
   style?: CSSProperties | undefined;
 }) {
   return (
     <>
-      <span style={{ fontSize: fontSize, lineHeight: lineHeight, ...style }}>
+      <span className={className ?? ""} style={{ fontSize: fontSize, lineHeight: lineHeight, ...style }}>
         <span className={styles.cyberLogoWordmark}>
           acm<span className={styles.cyberLogoWordmarkPeriod}>.</span>
           cyber
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,28 @@ import Image from "next/image";
 
 import styles from "styles/Home.module.scss";
 import CyberSeo from "@/components/CyberSeo";
+import CyberWordmark from "@/components/CyberWordmark";
 
 export default function HomePage() {
   return (
     <>
       <CyberSeo title="Home" />
-      <div className="page"></div>
+      <div className="page">
+        <div className={styles.welcomeHero}>
+          <div className={styles.cyberDiamondRelativeContainer}>
+            <div className={styles.cyberDiamond} />
+          </div>
+          <div className={styles.cyberDiamondInner}>
+            <p className={styles.welcomeTo}>Welcome to</p>
+            <CyberWordmark className={styles.acmCyber1} />
+            <div className={styles.elevatorPitchWrap}>
+              <p className={styles.elevatorPitch}>
+                We’re a student-run organization on a mission to <b>make cybersecurity simple and accessible to everyone</b>.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
