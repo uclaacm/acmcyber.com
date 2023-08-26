630ae8f839c58b63f2e17d20f6e5477e38f9778f
Benson Liu <bensonhliu@gmail.com>
2023-08-26 11:02:30 -0700

    feat: added new About page

---
+++ data/members.ts
@@ -0,0 +1,13 @@
+import { PersonInfoProps } from "./officers";
+
+const members: PersonInfoProps[] = [
+    {
+        name: "Ronak Badhe",
+        role: "Psi Beta Rho, Cyber Lab",
+        major: "Computer Science",
+        pronouns: "he/him",
+        photo: "ronak.webp",
+    }
+];
+
+export default members;
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 import officers, { PersonInfoProps } from "@/data/officers";
+import members from "@/data/members";
 
 import styles from "@/styles/About.module.scss";
 import CyberSeo from "@/components/CyberSeo";
@@ -13,30 +14,53 @@ export default function About() {
         description="Learn more about ACM Cyber at UCLA!"
       />
       <div className="page">
-        <h1>About</h1>
+        <div className="content">
+          <h1>About</h1>
+          <img
+            src="/images/cyber-symposium.png"
+            alt="ACM Cyber x ACM AI Spring 2023 projects symposium."
+            className={styles.aboutImage}
+          />
 
-        <h2>Who Are We</h2>
-        <p>
-          We are a group of hackers, CTFers and developers passionate about
-          cybersecurity. We break things for fun and work to improve our skills
-          in a variety of disciplines, whether it be rev, pwn, crypto, or
-          something entirely different. We aim to nurture the love of
-          cybersecurity in the students at UCLA.
-        </p>
+          <h2>Who Are We</h2>
+          <p>
+            We are a group of hackers & developers passionate about
+            cybersecurity. We break things for fun and work to improve our
+            skills in a variety of disciplines, whether it be reverse
+            engineering, binary exploitation, cryptography, or something
+            entirely different. We aim to nurture the love of cybersecurity in
+            the students at UCLA!
+          </p>
 
-        <h2>Team</h2>
+          <h2>Officers</h2>
 
-        <div className={styles.officersContainer}>
-          {officers.map((officer: PersonInfoProps, index: number) => (
-            <PersonInfo
-              key={index}
-              name={officer.name}
-              role={officer.role}
-              major={officer.major}
-              pronouns={officer.pronouns}
-              photo={officer.photo}
-            />
-          ))}
+          <div className={styles.officersContainer}>
+            {officers.map((officer: PersonInfoProps, index: number) => (
+              <PersonInfo
+                key={index}
+                name={officer.name}
+                role={officer.role}
+                major={officer.major}
+                pronouns={officer.pronouns}
+                photo={officer.photo}
+              />
+            ))}
+          </div>
+
+          <h2>Members</h2>
+
+          <div className={styles.officersContainer}>
+            {members.map((officer: PersonInfoProps, index: number) => (
+              <PersonInfo
+                key={index}
+                name={officer.name}
+                role={officer.role}
+                major={officer.major}
+                pronouns={officer.pronouns}
+                photo={officer.photo}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </>
@@ -48,16 +72,16 @@ function PersonInfo({ name, role, major, pronouns, photo }: PersonInfoProps) {
     <div className={styles.personInfo}>
       <Image
         className={styles.personImage}
-        src={"/images/officers/" + photo}
+        src={"/images/members/" + photo}
         alt={`Profile picture of ${name}`}
         width={300}
         height={300}
       />
       <h3>{name}</h3>
+      <p>{pronouns}</p>
       <p>
         <i>{role}</i>
       </p>
-      <p>{pronouns}</p>
       <p>{major}</p>
     </div>
   );
