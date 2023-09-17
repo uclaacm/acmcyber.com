d17b36f071b00aab9ba8e83cbb32e2cc4abf419e
vicchois <victoriachoi04@gmail.com>
2023-09-16 21:10:00 -0700

    Winter 23 Archives

--- data/archive.ts
+++ data/archive.ts
@@ -27,7 +27,7 @@ const archive: QuarterArchive[] = [
     series: [
       {
         name: "About Cyber", 
-        description: "Get to know us better!", 
+        description: "Get to know us better", 
         events: [
           {
             name: "Spring General Meeting", 
@@ -61,6 +61,21 @@ const archive: QuarterArchive[] = [
             graphicPath: "/images/archive/s23/mariokart.jpeg"
           },
           {
+            name: "Cyber x TeachLA: Cipher Lab", 
+            date: new Date("5/19/23"), 
+            location: "", 
+            description: 
+              "Teaching students about what a cipher is and showing \
+                them how they can build their own symmetric encryption \
+                cipher with Java. We'll cover the basic history of \
+                encryptions, their weaknesses, and give students an \
+                idea of how more advanced symmetric encryption \
+                algorithms work.", 
+            slides: "https://tinyurl.com/cipher-java-slides",
+            tags: ["ACM TeachLA", "teach", "colab", "java", "cipher"], 
+            graphicPath: "/images/archive/s23/javacipher.jpg"
+          },
+          {
             name: "Cyber x SWE: Career Panel",
             date: new Date("5/24/23"),
             location: "Math Sciences 5200",
@@ -88,7 +103,7 @@ const archive: QuarterArchive[] = [
       },
       {
         name: "Guest Speakers", 
-        description: "Learn from ACM Cyber alumni and cybersecurity professionals!", 
+        description: "Learn from ACM Cyber alumni and cybersecurity professionals", 
         events: [
           {
             name: "William Wang", 
@@ -148,7 +163,7 @@ const archive: QuarterArchive[] = [
       },
       {
         name: "Cyber Academy",
-        description: "Learn how to hack websites!",
+        description: "Learn how to hack websites",
         events: [
           {
             name: "Intro to Reverse Engineering",
@@ -262,7 +277,7 @@ const archive: QuarterArchive[] = [
       },
       {
         name: "Cyber Lab: Malware Lab", 
-        description: "Learn how to make your own rootkit!", 
+        description: "Learn how to make your own rootkit", 
         events: [
           {
             name: "Malware Lab", 
@@ -313,6 +328,215 @@ const archive: QuarterArchive[] = [
       },
     ],
   },
+  /* WINTER 2023 */
+  {
+    name: "Winter 2023", 
+    series: [
+      {
+        name: "About Cyber", 
+        description: "Get to know us better", 
+        events: [
+          {
+            name: "Winter General Meeting", 
+            date: new Date("1/18/23"), 
+            location: "Public Affairs 1234", 
+            description: 
+              "Cyber is back and better than ever, with \
+                even more brand-new, cool, exciting things for \
+                you'll to hack! Come out to meet our new team \
+                members adn find out what events we have in store \
+                for you this quarter! We'll also be previewing \
+                some of the web-hacking content that we will be going \
+                over during Cyber Academy, so be sure to bring a computer!", 
+            slides: "https://tinyurl.com/wintergm23", 
+            tags: ["gm"], 
+            graphicPath: "images/archive/w23/winter-gm.png",
+          },
+          {
+            name: "Cyber x Studio: Social", 
+            date: new Date ("1/27/23"), 
+            location: "Bruin Bear", 
+            description:
+              "Join us and ACM Studio for dinner to hangout with \
+                Cyber friends and meet new ones too! We'll first \
+                be meeting up at Bruin Bear and then walking to \
+                Westwood together to grab dinner adn boba afterwards.", 
+            tags: ["social", "colab", "studio"], 
+            graphicPath: "images/archive/w23/social.jpeg",
+          },
+          {
+            name: "LA CTF", 
+            date: new Date ("2/10/23"), 
+            location: "lactf.uclaacm.com", 
+            description: 
+              "We are happy to introduce LA CTF 2023, a \
+                42-hour cybersecurity event hosted by ACM Cyber \
+                at UCLA and team Psi Beta Rho. This event will take \
+                place from Feb 10-12 and features a variety of challenges \
+                and events for all skill levels. Everyone is welcome \
+                to participate and learn!", 
+            tags: ["LA CTF"], 
+            recording: "https://tinyurl.com/la-ctf-youtube-w23",
+            graphicPath: "images/archive/w23/la-ctf.jpeg"
+          },
+        ]
+      },
+      {
+        name: "Cyber Academy", 
+        description: "Learn how to hack websites", 
+        events: [
+          {
+            name: "Intro to Web Hacking", 
+            date: new Date("1/25/23"), 
+            location: "Boelter Hall 5249", 
+            description: 
+              "Are you interested in learning about the latest \
+                techniques and tools for web hacking? Our series of \
+                workshops is designed to introduce you to the world of \
+                web hacking, and help you develop the skills and knowledge \
+                you need to protect against cyber threats.", 
+            slides: "https://tinyurl.com/intro-to-web-w23", 
+            recording: "https://tinyurl.com/intro-web-hacking-recording",
+            tags: ["web", "hacking", "html", "css", "javascript"], 
+            graphicPath: "images/archive/w23/ca1.png",
+          },
+          {
+            name: "XSS & CSRF", 
+            date: new Date("2/1/23"), 
+            location: "Boelter Hall 5249", 
+            description: 
+              "Do you want to learn how a hacker once got one million \
+                friends on MySpace? Do you want to write a similar exploit \
+                yourself? Do you enjoy learning about random 3-letter acronyms \
+                that sound vaguely cybersecurity-related? Come join us to learn \
+                how to hack some (fake) websites and take over its users' (bots) \
+                browsers! No prior experience needed.", 
+            slides: "https://tinyurl.com/xss-csrf-w23", 
+            recording: "https://tinyurl.com/xss-csrf-recording",
+            tags: ["XSS", "CSRF"], 
+            graphicPath: "images/archive/w23/ca2.png",
+          },
+          {
+            name: "SQL Injection", 
+            date: new Date("2/15/23"), 
+            location: "Boelter Hall 5249", 
+            description: 
+              "As w23schools puts it 'SQL Injection is a code injection \
+                technique that might destroy your database.' Sounds fun. \
+                Jk we're all about ethical hacking! But if you want to \
+                explore SQL injection techniques and learn how to defend \
+                against them, come to our SQL injection workshop!", 
+            slides: "https://tinyurl.com/sql-injection-w23", 
+            tags: ["SQL"], 
+            graphicPath: "images/archive/w23/ca3.png",
+          },
+          {
+            name: "Intro to Web Auditing", 
+            date: new Date("2/22/23"), 
+            location: "Boelter Hall 5249", 
+            description: 
+              "Want to learn how to get paid for hacking websites? \
+              ACM Cyber is teaming up with Bruinwalk to present a \
+              unique real world experience to jump start your ethical \
+              hacking career. You will learn about what exactly are \
+              bug bounties, what exactly is auditing, and what tools \
+              and techniques are used by the professionals. After \
+              reviewing some of these techniques, you will get an \
+              exclusive chance to HACK the famous Bruinwalk website. \
+              No professors' reviews are safe from your technial skills!", 
+            slides: "https://tinyurl.com/web-auditing-w23", 
+            tags: ["web", "hacking", "colab"], 
+            graphicPath: "images/archive/w23/ca4.png",
+          },
+          {
+            name: "CTF After Dark Kickoff", 
+            date: new Date("3/1/23"), 
+            location: "Boelter Hall 5249", 
+            description: 
+              "Get ready to flex your cyber skills and bring your A-game \
+                to the ultimate test! CTF AFter Dark is back and better \
+                than ever! Come play our Capture the Flag (CTF) competition \
+                and show off all that you've learned from our workshops \
+                this winter. Winners will be rewarded, so don't miss out!", 
+            tags: ["CTF"], 
+            graphicPath: "images/archive/w23/ca5.png",
+          },
+        ]
+      },
+      {
+        name: "Special Topics", 
+        description: "Cyber in Other Fields", 
+        events: [
+          {
+            name: "AI Security", 
+            date: new Date("1/26/23"), 
+            location: "Kerckhoff Hall 131", 
+            description: 
+              "Calling all tech enthusiasts and AI aficionados! \
+                Join us to dive into the cutting-edge of modern security \
+                research, with a particular focus on adversarial attacks \
+                against neural networks. Join us for a day of learning, \
+                hacking, and fun as we explore the intersection of \
+                artificial intelligence and cybersecurity.", 
+            slides: "https://tinyurl.com/ai-security-slides", 
+            recording: "https://tinyurl.com/ai-security-recording",
+            tags: ["ACM AI", "colab", "AI"], 
+            graphicPath: "images/archive/w23/ai-security.png",
+          },
+          {
+            name: "Game Hacking", 
+            date: new Date("2/2/23"), 
+            location: "Kerckhoff Hall 131", 
+            description: 
+              "Ever wanted unlimited coins or infinite hp? Have \
+                you wished you could take out your in-game enemies in \
+                one sweep? Get ready to learn about the fundamentals of \
+                game hacking, some of the epic tools at your disposal, \
+                and test your skills on a game created by Studio.", 
+            slides: "https://tinyurl.com/CyberStudio23", 
+            recording: "https://tinyurl.com/game-hacking-recording",
+            tags: ["ACM Studio", "colab"], 
+            graphicPath: "images/archive/w23/game-hacking.png",
+          },
+          {
+            name: "Professor Yuan Tian", 
+            date: new Date("2/16/23"), 
+            location: "Kerckhoff Hall 131", 
+            description: 
+              "Professor Yuan Tian of UCLA's Electrical and \
+              Computer Engineering Department and \
+              Institute of Law, Technology, and Public Policy will be \
+              sharing her invaluable knowledge and expertise in this \
+              exciting talk. Her research focuses on developing \
+              secure and privacy-friendly computing platforms, with \
+              real-world applications integrated into platforms such as \
+              Android, Chrome, Azure, and iOS. Join us for an evening \
+              filled with thought-provoking discussions and valuable \
+              insights!", 
+            recording: "https://tinyurl.com/yuan-tian-talk",
+            tags: ["speaker", "industry", "research"], 
+            graphicPath: "images/archive/w23/yuan.png",
+          },
+          {
+            name: "Modern Cryptography", 
+            date: new Date("2/23/23"), 
+            location: "Kerckhoff Hall 131", 
+            description: 
+              "Come join us to learn about everythign cryptography, \
+                from current methods and attacks on these cryptosystems, \
+                to the mathematical and theoretical foundations of it all! \
+                We'll be covering a whole mix of things, including \
+                AES, RSA, diffie-hellman, one-way functions, secure \
+                hashes, and oblivious transfer!", 
+            slides: "https://tinyurl.com/modern-crypto-w23", 
+            recording: "https://tinyurl.com/modern-crypto-recording",
+            tags: ["cryptography"], 
+            graphicPath: "images/archive/w23/modern-crypto.png",
+          },
+        ]
+      },
+    ]
+  }
 ];
 
 export default archive;
