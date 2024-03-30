dbeae8dde56db7a044fa1e331908009a86c40b74
AVDestroyer <mr.arnav.vora@gmail.com>
2024-03-30 13:40:36 -0700

    fixed some more stuff

--- data/scripts/update_members.sh
+++ data/scripts/update_members.sh
@@ -5,7 +5,6 @@ extra_code="console.log(JSON.stringify(members));"
 sed 's/export//g' "$src" > "$new"
 sed -i '$d' "$new"
 echo "$extra_code" >> "$new"
-tsc "$new"
-node tmp/members-new.js > tmp/old-member-data
+npx ts-node --transpileOnly "$new" > tmp/old-member-data
 python3 update_members.py
 rm -rf tmp/
