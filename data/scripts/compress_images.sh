4b4a98ae1c8a54d9ffaa462db3eed879cf87fb75
AVDestroyer <mr.arnav.vora@gmail.com>
2024-04-15 17:35:56 -0700

    updated script, compressed images

--- data/scripts/compress_images.sh
+++ data/scripts/compress_images.sh
@@ -1,17 +1,14 @@
-if ! command -v cwebp &> /dev/null; then
-    echo "cwebp is not installed." >&2
-    exit 1
-fi
-
-for file in "../../public/images/members"/*.{jpg,png}; do
+for file in "../../public/images/members"/*.{jpg,png,webp}; do
     if [ -f "$file" ]; then
         filename=$(basename -- "$file")
         extension="${filename##*.}"
         filename_no_ext="${filename%.*}"
         out="../../public/images/members/$filename_no_ext.webp"
 
-        cwebp -q 80 "$file" -o "$out"
-        rm "$file"
+        convert "$file" -resize 400x400 "$out"
+        if [ "$extension" != "webp" ]; then
+            rm "$file"
+        fi
         echo "done"
         echo "$filename"
     fi
