0dfa2cc34fa30c61cc9657440d835421a04a782f
AVDestroyer <mr.arnav.vora@gmail.com>
2024-04-15 17:29:49 -0700

    made images webp, fixed scripts

--- data/scripts/compress_images.sh
+++ data/scripts/compress_images.sh
@@ -3,16 +3,19 @@ if ! command -v cwebp &> /dev/null; then
     exit 1
 fi
 
-for file in "../../public/images/members"/{.jpg,png}; do
+for file in "../../public/images/members"/*.{jpg,png}; do
     if [ -f "$file" ]; then
         filename=$(basename -- "$file")
         extension="${filename##*.}"
-        filename="${filename%.*}"
-        out="../../public/images/members/$filename.webp"
+        filename_no_ext="${filename%.*}"
+        out="../../public/images/members/$filename_no_ext.webp"
 
-        cwebp -q 80 "$file" -o "$output_file"
+        cwebp -q 80 "$file" -o "$out"
+        rm "$file"
+        echo "done"
+        echo "$filename"
     fi
 done
 
-sed 's/(jpg)|(png)/webp/g' "../members.ts" >"../members.ts"
+sed -i 's/\(jpg\|png\)/webp/g' ../members.ts
 
